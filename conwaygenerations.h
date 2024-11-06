3adbeefc4b93703cfddd091f7662ddf38108b268
Ali Raheem <ali-windows@shoryuken.me>
2024-11-06 10:21:40 +0000

    Arbitary bit lengths

--- conwaygenerations.h
+++ conwaygenerations.h
@@ -4,86 +4,104 @@ ConwayGenerations keeps track of how long a cell has been alive
 ConwayGenerations is optimized for memory usage targetting microcontrollers.
 Copyright Ali Raheem 2024 - https://github.com/ali-raheem/ConwayGenerations
 MIT Licensed
-File version: 2024-11-06 00:15 GMT
+File version: 2024-11-06 10:13 GMT
 */
+
+
 
 template<int rows, int cols>
 class ConwayGenerations {
 public:
-    ConwayGenerations(uint8_t (&state)[rows][cols])
-        : state(state), generation(0) {}
-
-    uint16_t generation;
-
-    void next() {
-        uint8_t prevRow[cols];
-        uint8_t firstRow[cols];
-        uint8_t curRow[cols];
-
-	memcpy(firstRow, state[0], cols);
-        memcpy(prevRow, state[rows - 1], cols);
-	// No fancy C++ for you arduino...
-        // std::copy(std::begin(state[0]), std::end(state[0]), firstRow);
-        // std::copy(std::begin(state[rows - 1]), std::end(state[rows - 1]), prevRow);
-
-        for(int i = 0; i < rows; i++) {
-	  memcpy(curRow, state[i], cols);
-          //  std::copy(std::begin(state[i]), std::end(state[i]), curRow);
-            uint8_t* nextRow = (i == rows - 1) ? firstRow : state[i + 1];
-
-            uint8_t sum_l = 0, sum_c = 0, sum_r = 0;
-
-            sum_l = !!getNibble(prevRow, 2 * cols - 1)
-                  + !!getNibble(curRow, 2 * cols - 1)
-                  + !!getNibble(nextRow, 2 * cols - 1);
-            sum_c = !!getNibble(prevRow, 0)
-                  + !!getNibble(curRow, 0)
-                  + !!getNibble(nextRow, 0);
-
-            for(int j = 0; j < 2 * cols; j++) {
-                size_t nextJ = (j + 1) % (2 * cols);
-                sum_r = !!getNibble(prevRow, nextJ)
-                      + !!getNibble(curRow, nextJ)
-                      + !!getNibble(nextRow, nextJ);
-
-                uint8_t currentState = getNibble(curRow, j);
-                uint8_t s = cgmin(getNextState(currentState, sum_l + sum_c + sum_r), 0b1111);
-                setNibble(state[i], j, s);
-
-                sum_l = sum_c;
-                sum_c = sum_r;
-            }
-	    memcpy(prevRow, curRow, cols);
-	    //            std::copy(std::begin(curRow), std::end(curRow), prevRow);
-        }
-        generation++;
+  ConwayGenerations(uint8_t (&state)[rows][cols], uint8_t bits)
+    : state(state), generation(0), bits(bits), colCells(8 / bits), mask((1 << bits) - 1) {}
+  
+  uint16_t generation;
+  uint8_t bits;
+  uint8_t colCells;
+  uint8_t mask;
+  uint8_t getCell(uint8_t* stateRow, uint8_t cellIndex) {
+    uint8_t i = cellIndex / colCells;
+    uint8_t pos = cellIndex % colCells;
+    uint8_t shift = bits * pos;
+    return (stateRow[i] >> shift) & mask;
+  }
+  
+  void setCell(uint8_t* stateRow, uint8_t cellIndex, uint8_t value) {
+    uint8_t i = cellIndex / colCells;
+    uint8_t pos = cellIndex % colCells;
+    uint8_t shift = bits * pos;
+    uint8_t mask_shifted = mask << shift;
+    value &= mask;
+    stateRow[i] = (stateRow[i] & ~mask_shifted) | (value << shift);
+  }
+  
+  uint8_t getTotalCellsInRow() const {
+    return cols * colCells;
+  }
+  void next() {
+    uint8_t prevRow[cols];
+    uint8_t firstRow[cols];
+    uint8_t curRow[cols];
+    
+    memcpy(firstRow, state[0], cols);
+    memcpy(prevRow, state[rows - 1], cols);
+    
+    for(int i = 0; i < rows; i++) {
+      memcpy(curRow, state[i], cols);
+      uint8_t* nextRow = (i == rows - 1) ? firstRow : state[i + 1];
+      
+      uint8_t sum_l = 0, sum_c = 0, sum_r = 0;
+      
+      // Adjusted to use totalCells instead of colCells * cols
+      uint8_t totalCells = colCells * cols;
+      sum_l = !!getNibble(prevRow, totalCells - 1)
+	+ !!getNibble(curRow, totalCells - 1)
+	+ !!getNibble(nextRow, totalCells - 1);
+      sum_c = !!getNibble(prevRow, 0)
+	+ !!getNibble(curRow, 0)
+	+ !!getNibble(nextRow, 0);
+      
+      for(int j = 0; j < totalCells; j++) {
+	size_t nextJ = (j + 1) % totalCells;
+	sum_r = !!getNibble(prevRow, nextJ)
+	  + !!getNibble(curRow, nextJ)
+	  + !!getNibble(nextRow, nextJ);
+	
+	uint8_t currentState = getNibble(curRow, j);
+	uint8_t s = cgmin(getNextState(currentState, sum_l + sum_c + sum_r), mask);
+	setNibble(state[i], j, s);
+	
+	sum_l = sum_c;
+	sum_c = sum_r;
+      }
+      memcpy(prevRow, curRow, cols);
     }
-
+    generation++;
+  }
+  
 private:
   uint8_t (&state)[rows][cols];
   
   template <typename T, typename U>
   inline uint8_t cgmin(T x, U cap) {
     T tcap = static_cast<T>(cap);
-    return (x < tcap)? x : tcap;
+    return (x < tcap) ? x : tcap;
   }
+  
   uint8_t getNibble(uint8_t* state, uint8_t nibble) {
-    uint8_t i = nibble / 2;
-    if (nibble % 2 == 0) {
-      return (state[i] >> 4) & 0x0F;
-    } else {
-      return state[i] & 0x0F;
-    }
+    uint8_t i = nibble / colCells;
+    uint8_t pos = nibble % colCells;
+    uint8_t shift = bits * pos;
+    return (state[i] >> shift) & mask;
   }
   
   void setNibble(uint8_t* state, uint8_t nibble, uint8_t value) {
-    uint8_t i = nibble / 2;
-    value &= 0x0F;
-        if (nibble % 2 == 0) {
-	  state[i] = (state[i] & 0x0F) | (value << 4);
-        } else {
-	  state[i] = (state[i] & 0xF0) | value;
-        }
+    uint8_t i = nibble / colCells;
+    uint8_t pos = nibble % colCells;
+    uint8_t shift = bits * pos;
+    uint8_t mask_shifted = mask << shift;
+    value &= mask;
+    state[i] = (state[i] & ~mask_shifted) | (value << shift);
   }
   
   uint8_t getNextState(uint8_t s, uint8_t sum) {
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ ConwayGenerations keeps track of how long a cell has been alive
 ConwayGenerations is optimized for memory usage targetting microcontrollers.
 Copyright Ali Raheem 2024 - https://github.com/ali-raheem/ConwayGenerations
 MIT Licensed
-File version: 2024-11-06 00:15 GMT
+File version: 2024-11-06 10:12 GMT
 */
 
 #include <iostream>
@@ -20,11 +20,12 @@ File version: 2024-11-06 00:15 GMT
 using namespace std;
 
 const int rows = 32;
-const int cols = 32;
+const int cols = 8;
 
 int main(int argc, char* argv[]) {
     uint8_t state[rows][cols];
-    ConwayGenerations<rows, cols> gol(state);
+    uint8_t bits = 2; // Or any other valid value (1, 2, 4)
+    ConwayGenerations<rows, cols> gol(state, bits);
     unsigned generations = 0;
     int max_generations = -1;
     int max_staleness = -1;
@@ -52,9 +53,13 @@ int main(int argc, char* argv[]) {
     mt19937 generator(seed);
     uniform_int_distribution<int> distribution(0, 1);
 
+    uint8_t totalCells = gol.getTotalCellsInRow();
+
+    // Initialize each cell individually
     for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            state[i][j] = distribution(generator);
+        for (uint8_t j = 0; j < totalCells; j++) {
+            uint8_t randomValue = distribution(generator);
+            gol.setCell(state[i], j, randomValue);
         }
     }
 
@@ -69,50 +74,24 @@ int main(int argc, char* argv[]) {
         gol.next();
 
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-	      uint8_t s = state[i][j] >> 4;
-	      s &= static_cast<uint8_t>(0b1111);
-		switch (s) {
-		case 0:
-		  cout << "   ";
-		  break;
-		case 1:
-		  cout << " - ";
-		  break;
-		case 2:
-		  cout << " + ";
-		  break;
-		case 3:
-		  cout << " = ";
-		  break;
-		case 4:
-		  cout << " # ";
-		  break;
-		default:
-		  cout << " @ ";
-		}
-               s = state[i][j];
-	      s &= static_cast<uint8_t>(0b1111);
-		switch (s) {
-		case 0:
-		  cout << "   ";
-		  break;
-		case 1:
-		  cout << " - ";
-		  break;
-		case 2:
-		  cout << " + ";
-		  break;
-		case 3:
-		  cout << " = ";
-		  break;
-		case 4:
-		  cout << " # ";
-		  break;
-		default:
-		  cout << " @ ";
-		}
-	    }
+            for (uint8_t j = 0; j < totalCells; j++) {
+                uint8_t s = gol.getCell(state[i], j);
+
+                // Adjust the display based on the cell value
+                switch (s) {
+                    case 0:
+                        cout << "   ";
+                        break;
+                    case 1:
+                        cout << " - ";
+                        break;
+                    case 2:
+                        cout << " = ";
+                        break;
+                    default:
+                        cout << " # ";
+                }
+            }
             cout << endl;
         }
         cout << endl;
@@ -128,7 +107,7 @@ int main(int argc, char* argv[]) {
             break;
 
         generations++;
-	this_thread::sleep_for(chrono::milliseconds(pause_duration));
+        this_thread::sleep_for(chrono::milliseconds(pause_duration));
     }
 
     return 0;
